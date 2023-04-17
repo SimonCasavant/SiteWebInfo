212c34524127b611fd5c38c898a60b5234ec5371
SimonCasavant <112723946+SimonCasavant@users.noreply.github.com>
2023-04-17 14:15:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("Test");
+
+function Youtube() {
+    document.getElementById("bg").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png')"
+}
