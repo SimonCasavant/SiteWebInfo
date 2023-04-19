36a588e000245df3dc45b9139b7387ca414c2599
SimonCasavant <112723946+SimonCasavant@users.noreply.github.com>
2023-04-19 14:03:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
 alert("Test");
 
 function Youtube() {
-    document.getElementById("bg").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png')"
+    document.getElementById("YouTube").style.backgroundImage = "url('https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg')"
+    document.getElementById("YouTube").style.color = "transparent"
+}
+
+function YoutubeLeave() {
+    document.getElementById("YouTube").style.backgroundImage = "none"
+    document.getElementById("YouTube").style.color = "white" 
+}
+
+function Twitch() {
+    document.getElementById("Twitch").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/3/3f/Twitch_Logo.jpg')"
+    document.getElementById("Twitch").style.color = "transparent"
+}
+
+function TwitchLeave() {
+    document.getElementById("Twitch").style.backgroundImage = "none"
+    document.getElementById("Twitch").style.color = "white"
+}
+
+function Twitter() {
+    document.getElementById("Twitter").style.backgroundImage = "url('https://education.howthemarketworks.com/wp-content/uploads/2019/04/twitter-logo.png')"
+    document.getElementById("Twitter").style.color = "transparent"
+}
+
+function TwitterLeave() {
+    document.getElementById("Twitter").style.backgroundImage = "none"
+    document.getElementById("Twitter").style.color = "white"
+}
+
+function Valorant() {
+    document.getElementById("Valorant").style.backgroundImage = "url('https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt79971d6ef53d8a5f/5e8cdeaa07387e0c9bfff0c5/IMAGE_4.jpg')"
+    document.getElementById("Valorant").style.color = "transparent"
+}
+
+function ValorantLeave() {
+    document.getElementById("Valorant").style.backgroundImage = "none"
+    document.getElementById("Valorant").style.color = "white"
+}
+
+function Speedrun() {
+    document.getElementById("Speedrun").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Speedrun.com_text_logo.svg/2560px-Speedrun.com_text_logo.svg.png')"
+    document.getElementById("Speedrun").style.color = "transparent"
+}
+
+function SpeedrunLeave() {
+    document.getElementById("Speedrun").style.backgroundImage = "none"
+    document.getElementById("Speedrun").style.color = "white"
 }
